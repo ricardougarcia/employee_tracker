d56cd94525ddea0fce9a1045c1dbe5b29566938b
Ricardo Garcia <ricardougarcia@gmail.com>
2021-11-17 14:33:54 -0500

    removed files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,13 @@
 -- Use this file to prepopulate the database to make development of individual features much easier
+
+INSERT INTO role (title, salary) 
+VALUES 
+("engineer", "75000"),
+("techLead", "85000"),
+("projectManager", "90000");
+
+INSERT INTO employee (first_name, last_name)
+VALUES 
+("sleve", "mcdichael"),
+("bobson", "dugnutt"),
+("todd", "bonzalez");
