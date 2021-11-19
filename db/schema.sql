5a0254b150a228422a554b21e60d2195ea57ff22
Ricardo Garcia <ricardougarcia@gmail.com>
2021-11-18 23:08:54 -0500

    updated seed

--- db/schema.sql
+++ db/schema.sql
@@ -34,29 +34,3 @@ CREATE TABLE employee (
     REFERENCES employee(id)
     );
     
-USE emp_tracker_db;
-INSERT INTO department (name)
-VALUES
-("engineering"),
-("sales"),
-("finance");
-
-INSERT INTO role (title, salary, department_id) 
-VALUES 
-("engineer", "75000", 1),
-("techLead", "85000", 1),
-("projectManager", "90000", 1),
-("salesperson", "80000", 2),
-("accountant", "65000", 3),
-("salesperson", "85000", 2),
-("accountant", "75000", 3);
-
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUES 
-("sleve", "mcdichael", NULL, 1),
-("bobson", "dugnutt", 1, 2),
-("todd", "bonzalez", 1, 3),
-("york", "peppermint", 2, 4),
-("cali", "fornia", 3, 5),
-("sweet", "caroline", 2, 6),
-("dane", "joe", 1, 7);
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,29 +3,25 @@ USE emp_tracker_db;
 INSERT INTO department (name)
 VALUES
 ("engineering"),
-("engineering"),
-("engineering"),
-("sales"),
-("finance"),
 ("sales"),
 ("finance");
 
-INSERT INTO role (title, salary) 
+INSERT INTO role (title, salary, department_id) 
 VALUES 
-("engineer", "75000"),
-("techLead", "85000"),
-("projectManager", "90000"),
-("salesperson", "80000"),
-("accountant", "65000"),
-("salesperson", "85000"),
-("accountant", "75000");
+("engineer", "75000", 1),
+("techLead", "85000", 1),
+("projectManager", "90000", 1),
+("salesperson", "80000", 2),
+("accountant", "65000", 3),
+("salesperson", "85000", 2),
+("accountant", "75000", 3);
 
-INSERT INTO employee (first_name, last_name)
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUES 
-("sleve", "mcdichael"),
-("bobson", "dugnutt"),
-("todd", "bonzalez"),
-("york", "peppermint"),
-("cali", "fornia"),
-("sweet", "caroline"),
-("dane", "joe");
+("sleve", "mcdichael", NULL, 1),
+("bobson", "dugnutt", 1, 2),
+("todd", "bonzalez", 1, 3),
+("york", "peppermint", 2, 4),
+("cali", "fornia", 3, 5),
+("sweet", "caroline", 2, 6),
+("dane", "joe", 1, 7);
